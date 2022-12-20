9c42812cab45e6d4507121af7cb1cd7f44f13ea1
Kenneth Onwubuya <82102207+RobotNinja15@users.noreply.github.com>
2022-12-19 22:57:32 -0500

    Update Audio.py

--- Audio.py
+++ Audio.py
@@ -1,10 +1,13 @@
 import pygame
 import numpy as np
 
-
 class Audio:
     def __init__(self):
         pygame.mixer.init()  # Initialize the Pygame mixer
+
+    def update_audio(self):
+        # Update the audio by playing a sound
+        self.play_sound(440, 1)  # For example, play a sound with frequency 440 Hz for 1 second
 
     def play_sound(self, frequency, duration):
         # Create a Pygame sound object and play it
@@ -15,4 +18,7 @@ class Audio:
         # Generate an array of sound samples with a given frequency and duration
         sample_rate = 44100
         num_samples = int(duration * sample_rate)
-        return (np.sin(2 * np.pi * np.arange(num_samples) * frequency / sample_rate)).astype(np.float32)
+        data = (np.sin(2 * np.pi * np.arange(num_samples) * frequency / sample_rate)).astype(np.float32)
+        # Return the data as a 2-dimensional array
+        return np.array([data, data]).T
+
