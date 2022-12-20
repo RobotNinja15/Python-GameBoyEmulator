eeb863cdd8b244aad0bee02fea66edce464ddb17
Kenneth Onwubuya <82102207+RobotNinja15@users.noreply.github.com>
2022-12-19 22:52:16 -0500

    Update Memory.py

--- Memory.py
+++ Memory.py
@@ -1,9 +1,13 @@
 class Memory:
     def __init__(self):
-        self.memory = [0] * 0xFFFF  # Create 64KB of memory
-
-    def read(self, address):
-        return self.memory[address]
+        self.memory = [0] * 65536  # Create a 65536-element list to store memory values
 
     def write(self, address, value):
-        self.memory[address] = value
+        if address >= 0 and address < len(self.memory):  # Check that the address is within bounds
+            self.memory[address] = value
+
+    def read(self, address):
+        if address >= 0 and address < len(self.memory):  # Check that the address is within bounds
+            return self.memory[address]
+
+
